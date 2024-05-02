8884ee2c15428d77e38107e7c952b5905ea595f4
Ngurah Deva <wirandanaputra@gmail.com>
2024-05-02 14:09:05 +0700

    [feat] add detail screen

--- Landmark/CircleImage.swift
+++ Landmark/CircleImage.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("turtlerock")
+            .resizable()
+            .frame(width: 200, height: 200)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
     CircleImage()
 }
+
