f968ea3f5a1a0880e5c4d05b4c825f136851c2ca
Ngurah Deva <wirandanaputra@gmail.com>
2024-05-02 14:08:30 +0700

    first commit

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by MacBook Air on 02/05/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmark/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmark
+//
+//  Created by MacBook Air on 02/05/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
